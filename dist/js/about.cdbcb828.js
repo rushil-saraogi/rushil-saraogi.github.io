ead3ae9f0ed8f5d10aea4e3033bd7da4200a8610
Rushil Saraogi <rushil@squareup.com>
2023-10-22 16:23:49 -0700

    Deploy build files

---
+++ dist/js/about.cdbcb828.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkrushil_saraogi"]=self["webpackChunkrushil_saraogi"]||[]).push([[443],{833:function(a,n,r){r.r(n),r.d(n,{default:function(){return h}});var s=r(3396);const u={class:"about"},i=(0,s._)("h1",null,"This is an about page",-1),t=[i];function e(a,n){return(0,s.wg)(),(0,s.iD)("div",u,t)}var c=r(89);const o={},l=(0,c.Z)(o,[["render",e]]);var h=l}}]);
+//# sourceMappingURL=about.cdbcb828.js.map
